7d5190ac4fb8fda397ae786ae6a8f7a51826ddd5
ernraff <103540977+ernraff@users.noreply.github.com>
2024-07-25 04:20:32 -0400

    Create cognito-stack.ts
    cdk config for user pool

---
+++ cognito-stack.ts
@@ -0,0 +1,47 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import { CognitoUserPoolsAuthorizer } from "aws-cdk-lib/aws-apigateway";
+
+export class CognitoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pool = new cognito.UserPool(this, "Pool");
+    userPoolName: "cdk-pool";
+    pool.addClient("app-client", {
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+        },
+        scopes: [
+          cognito.OAuthScope.OPENID,
+          cognito.OAuthScope.EMAIL,
+          cognito.OAuthScope.PHONE,
+        ],
+        callbackUrls: ["https://oauth.pstmn.io/v1/callback"],
+        logoutUrls: ["https://oauth.pstmn.io/v1/callback"],
+      },
+      supportedIdentityProviders: [
+        cognito.UserPoolClientIdentityProvider.FACEBOOK,
+        cognito.UserPoolClientIdentityProvider.COGNITO,
+      ],
+    });
+
+    const provider = new cognito.UserPoolIdentityProviderFacebook(
+      this,
+      "Facebook",
+      {
+        clientId: "911503144331593",
+        clientSecret: "f57502e9a5ebdeb34c3f84247a9aeb73",
+        userPool: pool,
+      }
+    );
+
+    pool.addDomain("CognitoDomain", {
+      cognitoDomain: {
+        domainPrefix: "cdk-restaurant-auth",
+      },
+    });
+  }
+}
